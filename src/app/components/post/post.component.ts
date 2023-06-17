a6c0347db645c58d6df69ed778741c48f4f67b9c
Bajan1408 <gianrodrigues@gmail.com>
2023-06-17 10:42:39 -0300

    Primeiro commit com o projeto na metade

---
+++ src/app/components/post/post.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+	tweetMessage:string = '';
+	tweetMaxLength:number = 240;
+	tweetRemain:number = this.tweetMaxLength;
+
+	changeTweet() {
+		this.tweetRemain = this.tweetMaxLength - this.tweetMessage.length;
+	}
+
+	sendTweet() {
+		alert(this.tweetMessage);
+	}
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
